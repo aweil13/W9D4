a98704626c7500c7bc0d3133ed784fb08e69ab76
alejandro <alejandroweil13@gmail.com>
2021-02-25 16:05:34 -0500

    Finished handleClick hopefully

--- skeleton/app/assets/javascripts/bundle.js
+++ skeleton/app/assets/javascripts/bundle.js
@@ -13,6 +13,7 @@ class FollowToggle {
         this.userId = this.$el.data('user-id');
         this.followState = this.$el.data('initial-follow-state');
         this.render();
+        this.$el.on('click', this.handleClick)
     }
 
     render() {
@@ -23,6 +24,29 @@ class FollowToggle {
             this.$el.text("UNFOLLOW")
         }
     }
+
+    handleClick(e) {
+        e.preventDefault();
+        if (this.followState === false) {
+            $.ajax({
+                method: "POST",
+                url: "/users/:id/follow",
+                dataType: "json",
+                success:  () => {this.followState = true;},
+                success: () => {this.render;}
+            });
+        }
+        else if (this.followState === true) {
+            $.ajax({
+                method: "DELETE",
+                url: "/users/:id/follow",
+                dataType: "json",
+                success:  () => {this.followState = false;},
+                success: () => {this.render;}
+            });
+        }
+    }
+
 }
 
 module.exports = FollowToggle;
